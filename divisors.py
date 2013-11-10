d24f77c2ae3635c7c5b8a7f2b395a6217348540d
Guest <guest-ksmUI8@meet.mit.edu>
2013-11-10 19:15:05 +0200

    divisors

--- divisors.py
+++ divisors.py
@@ -1,4 +1,10 @@
-n=raw_input("give me a number ")
-numbers = int(n)
-for numbers in xrange(n):	
-print 
+def divisors(n):
+	n = int(n)
+	for number in xrange(n):
+		if n%(number+1)==0:
+			print number+1 
+
+
+if __name__ == '__main__':
+	n=raw_input("give me a number ")
+	divisors(n)
